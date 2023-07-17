c20dfdbb1c0e0e0e2c23012bab56524741e0097c
Dilip Ramgopal <3260@xceedance.com>
2023-07-17 10:57:04 +0530

    3rd push

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Uber pickups in NYC New")
+st.title("Uber pickups in NYC New again")
 
 DATE_COLUMN = "date/time"
 DATA_URL = (
